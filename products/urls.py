d14e7bbcf1a78405120a32b70ba76106e699b6b7
Elvis Blessing <amadinelvis6@gmail.com>
2024-07-06 14:45:14 +0000

    Adding sorting template

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.all_product, name='all_product'),
+    path('', views.all_products, name='all_products'),
 ]
--- products/views.py
+++ products/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
 # Create your views here.
-def all_product(request):
+def all_products(request):
     """ A view to display all products"""
 
     products = Product.objects.all()
