d0f71243f63b2979952b2bcbc909f59eb9ff2a59
Elvis Blessing <amadinelvis6@gmail.com>
2024-07-06 03:22:18 +0000

    created views and url for our products and html template

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+
+from .models import Product, Category
 
 # Create your views here.
+def all_product(request):
+    """ A view to display all products"""
+
+    products = Product.objects.all()
+
+    context = {
+        products: 'products',
+    }
+
+    return render(request, 'produtcs/all_product.html', context)
