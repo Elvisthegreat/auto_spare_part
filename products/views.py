e288df75fe02d9dd2a096f92f3a59df10bdd861b
Elvis Blessing <amadinelvis6@gmail.com>
2024-07-07 13:48:53 +0000

    Tested display product and added customize css

--- products/views.py
+++ products/views.py
@@ -5,12 +5,15 @@ from .models import Product, Category
 
 # Create your views here.
 def all_products(request):
-    """ A view to display all products"""
+    """ A view to display all products, including sorting and search queries"""
 
     products = Product.objects.all()
+    # making sure the variables are defined for it to work properly
+    query = None
+    categories = None
 
     context = {
-        products: 'products',
+        'products': products,
     }
 
-    return render(request, 'produtcs/all_product.html', context)
+    return render(request, 'products/all_products.html', context)
