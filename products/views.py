a5fdd338e2deeee16fad90dc216e2860a5639163
Elvis Blessing <amadinelvis6@gmail.com>
2024-07-08 15:07:51 +0000

    HAndled a search query so user find products immediately

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q # To generate a search query
 
 
 from .models import Product, Category
@@ -11,6 +13,19 @@ def all_products(request):
     # making sure the variables are defined for it to work properly
     query = None
     categories = None
+
+    if request.GET:
+
+        """Handling the search query"""
+        if 'q' in request.GET: # remember the 'q' is the name in our input form in base.html
+            query = request.GET['q']
+            if not query:
+                messages.error(request, '“Oops! It looks like you forgot to enter a search query.”')
+                return redirect(reverse('all_products'))
+
+            # Return match query in either the product name or the description.
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
