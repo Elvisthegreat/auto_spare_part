2838f0727d817ce6e0220572a138df3a70e2d4b7
Elvis Blessing <amadinelvis6@gmail.com>
2024-07-17 17:32:35 +0000

    created the view for cache_checkout_data and added some comment on code

--- checkout/views.py
+++ checkout/views.py
@@ -14,8 +14,21 @@ import json
 
 # Create your views here.
 @require_POST
-
-
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
+    
 
 
 def checkout(request):
@@ -150,17 +163,20 @@ def checkout_success(request, order_number):
     """
     Handle successful checkouts
     """
-    save_info = request.session.get('save_info') # first check whether the user wanted to save their information by getting that from the session just like we get the shopping bag.
+    save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
      
      # Only if user is authenticated
     if request.user.is_authenticated:
-        profile = UserProfile.objects.get(user=request.user) # If authenticated, it retrieves the user’s profile associated with their account.
+        # If authenticated, it retrieves the user’s profile associated with their account.
+        profile = UserProfile.objects.get(user=request.user)
         # Attach the user's profile to the order
-        order.user_profile = profile # The retrieved profile is then attached to an order, and the order is saved.
+        order.user_profile = profile
         order.save()
 
-        # Save the user's info. If the save_info flag is true, it saves the user’s information from the order into their profile for future use.
+        """Save the user's info. If the save_info flag is true, it saves
+         the user’s information from the order into their profile for future use."""
+         
         if save_info:
             profile_data = {
                 'default_phone_number': order.phone_number,
