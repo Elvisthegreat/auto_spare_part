3b39f82488d3e2ca0a06697aeab9ad3483e852ab
Elvis Blessing <amadinelvis6@gmail.com>
2024-07-17 16:49:31 +0000

    created checkout_success html template imported some library into our views.py

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import (render,
-    redirect, reverse)
+    redirect, reverse, get_object_or_404, HttpResponse)
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
+from .models import Order, OrderLineItem
+from products.models import Product
 from bag.contexts import  bag_contents
 
 import stripe # imported stripe
@@ -74,9 +77,11 @@ def checkout(request):
                     order.delete()
                     return redirect(reverse('view_bag'))
 
-            # whether or not the user wanted to save their profile information to the session. And then redirect them to a new page"""
-            request.session['save_info'] = 'save_info' in request.POST
-            return redirect(reverse('checkout_success', args=[order.order_number]))
+                """whether or not the user wanted to save their profile
+                information to the session. And then redirect them to a new page"""
+
+                request.session['save_info'] = 'save_info' in request.POST
+                return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
@@ -96,8 +101,28 @@ def checkout(request):
         )
 
         # Since user now have profile user, users default delivery information to pre-fill the form on checkout page
+        if request.user.is_authenticated:
+            try:
+                """For example, we can fill in the full_name with the built
+                in get_full_name method on their user account to pre-fill the fields"""
 
-    order_form = OrderForm()
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        # Else render an empty form
+        else:
+            order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
@@ -109,4 +134,53 @@ def checkout(request):
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret
     }
+    return render(request, template, context)
+
+
+def checkout_success(request, order_number):
+    """checkout success view. 
+    This is simply going to take the order number and render a nice success page
+    letting the user know that their payment is complete."""
+
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info') # first check whether the user wanted to save their information by getting that from the session just like we get the shopping bag.
+    order = get_object_or_404(Order, order_number=order_number)
+     
+     # Only if user is authenticated
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user) # If authenticated, it retrieves the user’s profile associated with their account.
+        # Attach the user's profile to the order
+        order.user_profile = profile # The retrieved profile is then attached to an order, and the order is saved.
+        order.save()
+
+        # Save the user's info. If the save_info flag is true, it saves the user’s information from the order into their profile for future use.
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    # Delete the user shopping bag from the session
+    if 'bag' in request.session:
+        del request.session['bag']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+    }
+
     return render(request, template, context)
