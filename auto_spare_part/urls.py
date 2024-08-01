18452711b820014f99c4bde9032df734a640b483
Elvis Blessing <amadinelvis6@gmail.com>
2024-08-01 03:40:38 +0000

    Added a 404 error template

--- auto_spare_part/urls.py
+++ auto_spare_part/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from .views import handler404
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -30,3 +33,5 @@ urlpatterns = [
     path('about/', include('about.urls')),
     path('', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'auto_spare_part.views.handler404'
