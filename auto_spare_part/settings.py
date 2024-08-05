1a6c111c7bdbb67ea9b82096da72a1b00835d631
Elvis Blessing <amadinelvis6@gmail.com>
2024-08-05 17:08:54 +0000

    Added wishlist from in all products template

--- auto_spare_part/settings.py
+++ auto_spare_part/settings.py
@@ -28,8 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-elvisthegre-autosparepa-udtuuxrgaw4.ws.codeinstitute-ide.net', 'auto-spare-part-ad8a81e06f4e.herokuapp.com', 'localhost']
 
